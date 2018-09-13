4662dafeb77b8499000ed85be75c47839c427389
tinoweb <tino477@gmail.com>
2018-09-12 21:34:50 -0300

    site pronto pra subir no servidor

---
+++ index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>PASSAGENS.NET</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="tinoweb" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+	</head>
+	<body>
+
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-xs-2">
+							<div id="colorlib-logo">
+								<a href="index.html">PASSAGENS.NET</a>
+							<!-- 	<a href="" class="logo">
+									<img src="images/passagensNet.png" alt="logo">
+								</a> -->
+							</div>
+						</div>
+						<div class="col-xs-10 text-right menu-1 ">
+							<ul>
+								<li class="active"><a href="#" class="text-white">Home</a></li>
+								<li><a href="#colorlib-reservation" class="text-white">Simulação de Passagens</a></li>
+								
+								<li><a href="#" class="text-white">Contato</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<aside id="colorlib-hero">
+
+			<div class="flexslider">
+				<ul class="slides">
+					<li style="background-image: url(images/airplane.jpeg);">
+				   		<div class="overlay"></div>
+				   		<div class="container-fluid">
+				   			<div class="row">
+					   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+					   				<div class="slider-text-inner text-center">
+					   					<h2>Experimente</h2>
+					   					<h1>A melhor viajem de todos os tempos</h1>
+					   				</div>
+					   			</div>
+					   		</div>
+				   		</div>
+				   	</li>
+			   	
+			   	<li style="background-image: url(images/airport-indoors-people.jpeg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluid">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<!-- <h2>10 Days Cruises</h2>
+				   					<h1>From Greece to Spain</h1> -->
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			   	<li style="background-image: url(images/pexels-photo.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluids">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<!-- <h2>Explore our most tavel agency</h2>
+				   					<h1>Our Travel Agency</h1> -->
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+
+			   	<li style="background-image: url(images/img_bg_4.jpg);">
+			   		<div class="overlay"></div>
+			   		<div class="container-fluids">
+			   			<div class="row">
+				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
+				   				<div class="slider-text-inner text-center">
+				   					<!-- <h2>Explore our most tavel agency</h2>
+				   					<h1>Our Travel Agency</h1> -->
+				   				</div>
+				   			</div>
+				   		</div>
+			   		</div>
+			   	</li>
+			  	</ul>
+		  	</div>
+		</aside>
+
+		<div id="colorlib-reservation" style="background-color: #000000;" class="fadeInUp">
+			<div class="row">
+				<div class="col-md-6 text-center">
+					<div class="services">
+						<span class="icon">
+							<!-- <i class="flaticon-around"></i> -->
+							<a href="" class="logo">
+								<img src="images/passagensNet.png" alt="logo" style="width: 357px;border-radius: 4px;position: relative;top: 0px;">
+							</a>
+						</span>
+						
+						<p class="font25 text-white">Preencha o formulario para solicitar o valor das promoções das passagens nas datas desejadas. Entraremos em contato o mais rápido possivel para lhe passar todas a Informações</p>
+					</div>
+				</div>
+				<div class="col-md-6">
+				<div class="search-wrap">
+					<div class="container">
+						<ul class="nav nav-tabs">
+							<li class="active"><a data-toggle="tab" href="#flight"><i class="flaticon-plane"></i> Simulação de Passagens</a></li>
+						</ul>
+					</div>
+					<div class="tab-content">
+						<div id="flight" class="tab-pane fade in active">
+							<form id="simulaPassageForm" method="post" class="colorlib-form">
+				              	<div class="row">
+				              		
+				              		<div class="col-md-6">
+					              	 	<div class="form-group">
+					                    <label for="nome">Nome</label>
+					                    <div class="form-field">
+					                      <input type="text" required id="nome" id="nome" name="nome" class="form-control" placeholder="Seu nome">
+					                    </div>
+					                  </div>
+					              	</div>
+					              	<div class="col-md-6">
+					              	 	<div class="form-group">
+					                    <label for="nome">Email</label>
+					                    <div class="form-field">
+					                      <input type="text" required id="email" id="email" name="email" class="form-control" placeholder="Seu Email">
+					                    </div>
+					                  </div>
+					              	</div>
+					              	<div class="col-md-12">
+					              	 	<div class="form-group">
+					                    <label for="telefone">Watsapp</label>
+					                    <div class="form-field">
+					                      <input type="text" required id="telefone" name="telefone" class="form-control" placeholder="Celular">
+					                    </div>
+					                  </div>
+					              	</div>
+					              	
+					              	<div class="col-md-6">
+					              	 	<div class="form-group">
+					                    <label for="date">Origem</label>
+					                    <div class="form-field">
+					                      <input type="text" name="origem" id="origem" class="form-control" placeholder="Origem">
+					                    </div>
+					                  </div>
+					              	</div>
+					              	<div class="col-md-6">
+					              	 	<div class="form-group">
+					                    <label for="date">Destino</label>
+					                    <div class="form-field">
+					                      <input type="text" name="destino" id="destino" class="form-control" placeholder="Destino">
+					                    </div>
+					                  </div>
+					              	</div>
+
+					              	<div class="col-md-4">
+					              	 	<div class="form-group">
+					                    <label for="nome">Adultos </label>
+					                    <div class="form-field">
+					                      <input type="number" id="passAdulto" name="passAdulto" class="form-control" placeholder="Adulto">
+					                    </div>
+					                  </div>
+					              	</div>
+					              	<div class="col-md-4">
+					              	 	<div class="form-group">
+					                    <label for="telefone">Crianças</label>
+					                    <div class="form-field">
+					                      <input type="number" id="passCrianca" name="passCrianca" class="form-control" placeholder="Crianças">
+					                    </div>
+					                  </div>
+					              	</div>
+					              	<div class="col-md-4">
+					              	 	<div class="form-group">
+					                    <label for="telefone">Bebês</label>
+					                    <div class="form-field">
+					                      <input type="number" id="passBebe" name="passBebe" class="form-control" placeholder="Bebês">
+					                    </div>
+					                  </div>
+					              	</div>
+
+					                <div class="col-md-12">
+					                  <div class="form-group">
+					                    <label for="date">Data Ida:</label>
+					                    <div class="form-field">
+					                      <i class="icon icon-calendar2"></i>
+					                      <input type="text" name="dataIda" id="dataida" class="form-control date" placeholder="Data de Ida">
+					                    </div>
+					                  </div>
+					                </div>
+					                <div class="col-md-12">
+					                  <div class="form-group">
+					                    <label for="date">Data Volta:</label>
+					                    <div class="form-field">
+					                      <i class="icon icon-calendar2"></i>
+					                      <input type="text" name="dataChegada" id="dataChegada" class="form-control date" placeholder="Data de Volta">
+					                    </div>
+					                  </div>
+					                </div>
+					                <div class="col-md-12" id="btnEnviar">
+					                	<button class="btn btn-primary btn-block">Enviar</button>
+					                </div>
+					                <div id="loadingStatus" class="loadingStatus col-md-12 text-center hide">
+									    <img src="images/loader.gif" alt="" style="width: 60px;" class="flexslider">
+									</div>
+				              </div>
+				            </form>
+			         	</div>
+		         	</div>
+				</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="colorlib-subscribe" style="background-image: url(images/passagensNet.png);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
+						<h2>Subscreva para Novidades</h2>
+						<p>Inscreva seu email para receber ultimas novidades e ofertas</p>
+						<form class="form-inline qbstp-header-subscribe">
+							<div class="row">
+								<div class="col-md-12 col-md-offset-0">
+									<div class="form-group">
+										<input type="text" class="form-control" id="email" placeholder="Adicione seu email">
+										<button type="submit" class="btn btn-primary">Subscreva-se</button>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 class="modal-title" id="myModalLabel">Simulação Enviada com sucesso</h4>
+		      </div>
+		      <div class="modal-body">
+		      	<div class="alert alert-success alert-dismissible fade in" role="alert"> 
+		      		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		      			<span aria-hidden="true">×</span>
+		      		</button> 
+		      		<span id="resposta"></span>
+		      		Entraremos em contato o mais breve possivel. 
+		      	</div>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+		<style>
+			.pt{
+				list-style: none;
+			    padding: 12px;
+			    /*max-width: 240px;*/
+			}
+		</style>
+
+		<footer id="colorlib-footer" role="contentinfo">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 text-center">
+						<a href="" class="logo">
+							<img src="images/cadastur.jpeg" alt="logo" style="width: 161px;border-radius: 4px;position: relative;top: 0px;">
+						</a>
+					</div>
+					<div class="col-md-6">
+						<ul style="margin-top: 26px;">
+							<li class="pt"><strong>Contato:</strong> + 55 (19) 99429-6826</li>
+							<li class="pt"><strong>CNPJ:</strong> 30.490.617/0001-01</li>
+							<li class="pt"><strong>Endereço:</strong> Avenida Floriano Peixoto 737 - centro - Artur Nogueira </li>
+							<li class="pt"><strong>SP CEP:</strong> 13160-000 </li>
+						</ul>
+					</div>
+					<div class="col-md-12 text-center">
+						<br>
+						<p>
+							Copyright &copy;<script>document.write(new Date().getFullYear());</script> Passagens.net Viajens Online, Todos os direitos reservados  <i class="icon-heart2" aria-hidden="true"></i> pelo <a href="http://softlike.com.br" target="_blank">Softlike</a>
+							</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+z	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Date Picker -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+
+	</body>
+</html>
---
+++ sendEmail.php
@@ -0,0 +1,85 @@
+<?php 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer;
+
+$mail->IsSMTP();     
+$mail->CharSet = 'UTF-8';                
+$mail->SMTPAuth   = true;    
+
+$mail->Host       = "smtp.gmail.com";       
+$mail->Port 	  = 587;  
+$mail->SMTPSecure = 'tls';                  
+$mail->Username   = "tino477@gmail.com";    
+$mail->Password   = "lopesgomes102";     
+$mail->From     = "tino477@gmail.com";
+$mail->FromName = "Passagens.net";
+$mail->addAddress("tino477@gmail.com");
+
+
+// $mail->Host       = "smtp.live.com";       
+// $mail->Port       = 587;  
+// $mail->SMTPSecure = 'tls';                  
+// $mail->Username   = "passagens.net@outlook.com.br";    
+// $mail->Password   = "AYLAesther24";    
+
+// From email address and name
+// $mail->From  = "passagens.net@outlook.com.br";
+// $mail->FromName = "Passagens.net";
+
+//To address and name
+
+// $mail->addAddress("passagens.net@outlook.com.br");
+// $mail->addReplyTo("passagens.net@outlook.com.br", "Reply");
+$mail->isHTML(true);
+
+	$data        = $_POST;
+    
+    $name        = $data['nome'];
+    $email       = $data['email'];
+    $telefone    = $data['telefone'];
+    $origem         = $data['origem'];
+    $destino        = $data['destino'];
+    $passAdulto      = $data['passAdulto'];
+    $passCrianca     = $data['passCrianca'];
+    $passBebe        = $data['passBebe'];
+    $dataIda     = $data['dataIda'];
+    $dataChegada = $data['dataChegada'];
+    
+    $body = 
+    	'
+		   <strong>Nome:</strong>         ' . $name .         '<br>
+           <strong>Email:</strong>        ' . $email .        '<br>
+		   <strong>Telefone:</strong>     ' . $telefone .     '<br>
+		   <strong>Origem:</strong>       ' . $origem .       '<br>
+           <strong>Destino:</strong>      ' . $destino .      '<br>
+		   <strong>Passgem Adulto:</strong>   ' . $passAdulto .   '<br>
+           <strong>Passgem Criança:</strong>  ' . $passCrianca .  '<br>
+           <strong>Passgem Bebe:</strong>     ' . $passBebe .     '<br>
+           <strong>Data Ida:</strong>     ' . $dataIda .      '<br>
+		   <strong>Data chegada:</strong> ' . $dataChegada .  '<br>
+		';
+
+
+$mail->Subject = "Simulação passagem";
+$mail->Body = '<p> Olá Olan Novais estou fazendo simulações de passagens no site passagem.net
+		   Os dados para contato são: </p>' .$body;
+
+if(!$mail->send()) 
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} 
+else 
+{
+    echo "Mensagem enviado com sucesso";
+}
+
+
+
+
